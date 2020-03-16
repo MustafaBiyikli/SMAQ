0f1e80f09b04863b16ff478958fc41644fbaa3b6
MustafaBiyikli <biyiklimustafa96@gmail.com>
2020-03-16 00:31:20 +0000

    Added unit test for ADS1115

--- Software/Real-Time/unitTest.cpp
+++ Software/Real-Time/unitTest.cpp
@@ -3,27 +3,49 @@
 #include "wiringPiI2C.h"
 #include "BME280.h"
 #include "VCNL4010.h"
+#include "ADS1115.h"
 
 using namespace std;
 
-int testI2Ccommunication()
+int testI2Ccommunication_BME280()
 {
     int fd_BME280 = wiringPiI2CSetup(BME280_ADDRESS);
-    int fd_VCNL4010 = wiringPiI2CSetup(VCNL4010_ADDRESS);
-
-    if (fd_BME280 < 0) {
+    if (fd_BME280 < 0)
+    {
         throw runtime_error("BME280 communication failed");
         return -1;
     }
-    if (fd_VCNL4010 < 0) {
+
+    return 0;
+}
+
+int testI2Ccommunication_VCNL4010()
+{
+    int fd_VCNL4010 = wiringPiI2CSetup(VCNL4010_ADDRESS);
+    if (fd_VCNL4010 < 0)
+    {
         throw runtime_error("VCNL4010 communication failed");
         return -1;
     }
+
+    return 0;
+}
+
+int testI2Ccommunication_ADS1115()
+{
+    int fd_ADS1115 = wiringPiI2CSetup(ADS1115_ADDRESS);
+    if (fd_ADS1115 < 0)
+    {
+        throw runtime_error("ADS1115 communication failed");
+        return -1;
+    }
+
     return 0;
 }
 
 int main()
 {
-    testI2Ccommunication();
+    testI2Ccommunication_BME280();
+    testI2Ccommunication_VCNL4010();
+    testI2Ccommunication_ADS1115();
 }
-
