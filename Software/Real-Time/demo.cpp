9975b01538dce8f673f90fdf1e2225283adc2602
MustafaBiyikli <biyiklimustafa96@gmail.com>
2020-03-23 15:26:57 +0000

    added timeStamp in hasSample

--- Software/Real-Time/demo.cpp
+++ Software/Real-Time/demo.cpp
@@ -5,9 +5,10 @@ using namespace std;
 
 class printCallback : public Callback
 {
-    virtual void hasSample(int ambientLight, int proximity, float temperature, float pressure, float humidity, float altitude,
+    virtual void hasSample(time_t timeStamp, int ambientLight, int proximity, float temperature, float pressure, float humidity, float altitude,
                            int microphone, int nh3, int no2, int co)
     {
+        cout << "timeStamp: " << timeStamp << endl;
         cout << "ambientLight: " << ambientLight << " lux | proximity: " << proximity << endl;
         cout << "temperature: " << temperature << " C | pressure: " << pressure << " hPa" << endl;
         cout << "humidity: " << humidity << " % | altitude: " << altitude << " m" << endl;
