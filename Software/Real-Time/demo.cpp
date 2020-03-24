e9227b85e8753d4e7104517bff3ef6b658e48c9a
MustafaBiyikli <biyiklimustafa96@gmail.com>
2020-03-24 01:35:33 +0000

    added udp transmitter compatibility

--- Software/Real-Time/demo.cpp
+++ Software/Real-Time/demo.cpp
@@ -8,14 +8,14 @@ class printCallback : public Callback
     virtual void hasSample(time_t timeStamp, int ambientLight, int proximity, float temperature, float pressure, float humidity, float altitude,
                            int microphone, int nh3, int no2, int co)
     {
-        cout << "timeStamp: " << timeStamp << endl;
-        cout << "ambientLight: " << ambientLight << " lux | proximity: " << proximity << endl;
-        cout << "temperature: " << temperature << " C | pressure: " << pressure << " hPa" << endl;
-        cout << "humidity: " << humidity << " % | altitude: " << altitude << " m" << endl;
-        cout << "microphone: " << microphone << endl;
-        cout << "NH3: " << nh3 << endl;
-        cout << "NO2: " << no2 << endl;
-        cout << "CO: " << co << endl;
+        cout << "timeStamp:" << timeStamp << endl;
+        cout << "ambientLight:" << ambientLight << "|proximity:" << proximity << endl;
+        cout << "temperature:" << temperature << "|pressure:" << pressure << endl;
+        cout << "humidity:" << humidity << "|altitude:" << altitude << endl;
+        cout << "microphone:" << microphone << endl;
+        cout << "NH3:" << nh3 << endl;
+        cout << "NO2:" << no2 << endl;
+        cout << "CO:" << co << endl;
         cout << "---------------------------------------" << endl;
     }
 };
