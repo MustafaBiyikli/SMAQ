aa81fa7a34b7d1825f0e2203605ee4fffc2653e7
MustafaBiyikli <biyiklimustafa96@gmail.com>
2020-03-28 13:48:43 +0000

    implemented CSV formatter for max data length

--- Software/Website/udp_receive.js
+++ Software/Website/udp_receive.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
 const dgram = require("dgram");
+const writetoCSV = require("./writetoCSV");
 const ab2str = require("arraybuffer-to-string");
 
 var server = dgram.createSocket("udp4");
 
 const port = 65000;
+var counter = 0;
+const maxCSVLength = 100;
 
 server.on("error", err => {
     console.log(`server error:\n${err.message}\n${err.stack}`);
@@ -15,66 +18,37 @@ server.on("listening", function() {
     var address = server.address();
 });
 
+// TODO: Improve, user loses data here. (see writetoCSV.js)
+writetoCSV.clearCSV([
+    "./csv/ambient.csv",
+    "./csv/gas.csv",
+    "./csv/hum.csv",
+    "./csv/mic.csv",
+    "./csv/press.csv",
+    "./csv/temp.csv"
+]);
+
 server.on("message", function(message, remote) {
     var smaqData = ab2str(message);
     let [tStamp, ALS, PR, T, P, H, A, MIC, NH3, NO2, CO] = smaqData.split(",");
 
-    fs.appendFile(
-        "./csv/ambient.csv",
-        `${"\n" + new Date() + "," + ALS}`,
-        "utf-8",
-        err => {
-            if (err) throw err.message;
-        }
-    );
-
-    fs.appendFile(
-        "./csv/press.csv",
-        `${"\n" + new Date() + "," + P}`,
-        "utf-8",
-        err => {
-            if (err) throw err.message;
-        }
-    );
-
-    fs.appendFile(
-        "./csv/mic.csv",
-        `${"\n" + new Date() + "," + MIC}`,
-        "utf-8",
-        err => {
-            if (err) throw err.message;
-        }
-    );
-
-    fs.appendFile(
+    // Append data to CSV as received
+    writetoCSV.writeFormatData("./csv/ambient.csv", ALS, maxCSVLength, counter);
+    writetoCSV.writeFormatData(
         "./csv/gas.csv",
-        `${"\n" + new Date() + "," + NH3 + "," + NO2 + "," + CO}`,
-        "utf-8",
-        err => {
-            if (err) throw err.message;
-        }
+        `${NH3},${NO2},${CO}`,
+        maxCSVLength,
+        counter
     );
+    writetoCSV.writeFormatData("./csv/hum.csv", H, maxCSVLength, counter);
+    writetoCSV.writeFormatData("./csv/mic.csv", MIC, maxCSVLength, counter);
+    writetoCSV.writeFormatData("./csv/press.csv", P, maxCSVLength, counter);
+    writetoCSV.writeFormatData("./csv/temp.csv", T, maxCSVLength, counter);
 
-    fs.appendFile(
-        "./csv/temp.csv",
-        `${"\n" + new Date() + "," + T}`,
-        "utf-8",
-        err => {
-            if (err) throw err.message;
-        }
-    );
-
-    fs.appendFile(
-        "./csv/hum.csv",
-        `${"\n" + new Date() + "," + H}`,
-        "utf-8",
-        err => {
-            if (err) throw err.message;
-        }
-    );
+    counter++;
 });
 
 server.bind({
     host: "localhost",
-    port: 65000
+    port: port
 });
