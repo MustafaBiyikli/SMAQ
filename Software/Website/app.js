289b50b25058e26b7bf9c600e7b03471fe5380ab
KasiaLenard <kasia.lenard@gmail.com>
2020-04-04 15:16:11 +0100

    Added config.env

--- Software/Website/app.js
+++ Software/Website/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = require("./router");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use(express.static(__dirname));
 
