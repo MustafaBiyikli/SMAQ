22e38470a35d3ce082e8eee9bc274a32e89c1c9f
MustafaBiyikli <60039265+MustafaBiyikli@users.noreply.github.com>
2020-04-07 21:58:57 +0100

    Delete testAlerts.js

--- Software/Website/testAlerts.js
+++
@@ -1,9 +0,0 @@
-const writetoAlerts = require("./writetoAlerts");
-const fs = require("fs");
-
-setInterval(function() {
-    var [tStamp, ALS, PR, T, P, H, A, MIC, NH3, NO2, CO] = fs
-        .readFileSync("./csv/api.csv", "utf-8")
-        .split(",");
-    writetoAlerts.alertHandler(tStamp, T, P, H, NH3, NO2, CO);
-}, 1000);
