f7a285d5bd3e94772dc7d59e84d38e81b03d2a14
MustafaBiyikli <biyiklimustafa96@gmail.com>
2020-04-13 02:10:18 +0100

    drawerLayout and url load listener added

--- Software/Android-App/app/src/main/java/com/samadevelopment/mustafa/smaq/smaqActivity.java
+++ Software/Android-App/app/src/main/java/com/samadevelopment/mustafa/smaq/smaqActivity.java
@@ -1,23 +1,33 @@
 package com.samadevelopment.mustafa.smaq;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class smaqActivity extends AppCompatActivity {
 
     Intent mainActivity;
     String urlAddress;
     WebView webView;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +40,40 @@ public class smaqActivity extends AppCompatActivity {
         webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(urlAddress + "/app&dashboard");
+        webView.bringToFront();
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                drawerLayout.closeDrawer(Gravity.RIGHT);
+            }
+        });
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+                webView.bringToFront();
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -43,10 +85,43 @@ public class smaqActivity extends AppCompatActivity {
                         webView.loadUrl(urlAddress + "/app/v1/alerts");
                         break;
                     case R.id.options:
+                        drawerLayout.openDrawer(Gravity.RIGHT);
+                        drawerLayout.bringToFront();
                         break;
                 }
                 return true;
             }
         });
+
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.settings:
+                        webView.loadUrl(urlAddress + "/app/v1/settings");
+                        break;
+                    case R.id.about:
+                        webView.loadUrl(urlAddress + "/app/v1/about");
+                        break;
+                    case R.id.contact:
+                        webView.loadUrl(urlAddress + "/app/v1/contact");
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Rect viewRect = new Rect();
+        navigationView.getGlobalVisibleRect(viewRect);
+        if (!viewRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+            drawerLayout.closeDrawer(Gravity.RIGHT);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
